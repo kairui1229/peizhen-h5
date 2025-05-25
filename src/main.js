1f65d2e0417533dbf3598a594743dd7da811f208
欧阳凯锐 <2889626978@qq.com>
2025-05-25 16:10:11 +0800

    登录页面效果显示

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
-import './style.css'
 import { Button } from 'vant';
 import { Tabbar, TabbarItem } from 'vant';
+import { Form, Field, CellGroup } from 'vant';
 import 'vant/lib/index.css';
 import router from './router'
 import App from './App.vue'
@@ -10,5 +10,8 @@ const app = createApp(App)
 app.use(Button)
 app.use(Tabbar);
 app.use(TabbarItem);
+app.use(Form);
+app.use(Field);
+app.use(CellGroup);
 app.use(router)
 app.mount('#app')
--- src/pages/login/index.vue
+++ src/pages/login/index.vue
@@ -1,13 +1,46 @@
 <template>
-  <div>
-login
-  </div>
+  <h1>用户登录</h1>
+  <van-form @submit="onSubmit">
+     <van-cell-group inset>
+       <van-field 
+       v-model="form.username" 
+       name="用户名" 
+       label="用户名" 
+       placeholder="用户名" 
+       :rules="[{ required: true, message: '请填写用户名' }]"
+       />
+       <van-field 
+       v-model="form.password" 
+       type="password" 
+       name="密码" 
+       label="密码" 
+       placeholder="密码" 
+       :rules="[{ required: true, message: '请填写密码' }]"
+       />
+     </van-cell-group>
+     <div class="btn">
+      <van-button round block type="primary" native-type="submit">提交</van-button>
+     </div>
+  </van-form>
 </template>
 
 <script setup>
+import { ref,reactive } from 'vue'
 
+const form  = reactive({
+  username:'',
+  password:''
+})
+const onSubmit = () => {
+ 
+}
 </script>
 
 <style lang="less" scoped>
-
+h1{
+  text-align: center;
+}
+.btn{
+  margin: 16px;
+}
 </style>
