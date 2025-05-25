3c74bc8eb4824e522f7d62a114275d58f22bc8a5
欧阳凯锐 <2889626978@qq.com>
2025-05-25 15:49:34 +0800

    底部标签

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 import { createApp } from 'vue'
 import './style.css'
 import { Button } from 'vant';
+import { Tabbar, TabbarItem } from 'vant';
 import 'vant/lib/index.css';
 import router from './router'
 import App from './App.vue'
 
 const app = createApp(App)
 app.use(Button)
+app.use(Tabbar);
+app.use(TabbarItem);
 app.use(router)
 app.mount('#app')
--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -1,9 +1,29 @@
 <template>
-    <router-view></router-view>
+  <router-view></router-view>
+  <van-tabbar v-model="active">
+    <van-tabbar-item 
+    v-for="item in router.options.routes[0].children" 
+    :key="item.path" 
+    :url="`#/${item.path}`"
+    :icon="item.meta.icon"
+    >
+     {{ item.meta.name }}
+    </van-tabbar-item>
+  </van-tabbar>
 </template>
 
 <script setup>
+import { ref,onMounted } from 'vue'
+import { useRouter, useRoute} from 'vue-router'
 
+const router = useRouter()
+const route = useRoute()
+const active = ref(0)
+
+onMounted(()=>{
+  const data = router.options.routes[0]
+  active.value = data.children.findIndex(item=> item.path === route.path)
+})
 </script>
 
 <style lang="less" scoped>
