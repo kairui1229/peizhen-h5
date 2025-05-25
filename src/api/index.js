0a9be3a934c8624f713f1057924140db5151977f
欧阳凯锐 <2889626978@qq.com>
2025-05-25 17:51:13 +0800

    首页页面效果实现

--- src/api/index.js
+++ src/api/index.js
@@ -3,5 +3,9 @@ import request from '@/utils/request'
 export default {
   login(data){
     return request.post('/login', data)
+  },
+  //首页信息
+  index(){
+    return request.get('/Index/index')
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import { createApp } from 'vue'
 import { Button } from 'vant';
 import { Tabbar, TabbarItem } from 'vant';
-import { Form, Field, CellGroup } from 'vant';
+import { Form, Field } from 'vant';
+import { Search } from 'vant';
+import { Icon } from 'vant'
+import { Swipe, SwipeItem } from 'vant';
+import { Image as VanImage } from 'vant';
+import { Cell, CellGroup } from 'vant';
+import { Col, Row } from 'vant';
 import 'vant/lib/index.css';
 import router from './router'
 import api from './api'
@@ -15,5 +21,13 @@ app.use(TabbarItem);
 app.use(Form);
 app.use(Field);
 app.use(CellGroup);
+app.use(Icon);
+app.use(Search);
+app.use(Swipe);
+app.use(SwipeItem);
+app.use(VanImage);
+app.use(Cell);
+app.use(Col);
+app.use(Row);
 app.use(router)
 app.mount('#app')
